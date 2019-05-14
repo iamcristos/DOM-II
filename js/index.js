9c9d1caec2bc46ee65d4066f6f358fb87e31ead7
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-14 15:30:18 +0100

    implemented db click

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 // Your code goes here
-const logoHeading = document.querySelector('.logo-heading');
-logoHeading.addEventListener('mouseover', (e)=>{
+let logooHeading = document.querySelector('.logo-heading');
+logooHeading.addEventListener('mouseover', (e)=>{
     e.target.classList.toggle('focusClass')
+});
+
+// keydown event
+
+const heade = document.querySelectorAll('a');
+heade.forEach(a => {
+    a.addEventListener('dblclick', (e)=>{
+        console.log(e.target)
+        e.target.classList.toggle('keydownClass')
+    })
 })
+
+const p = document.querySelectorAll('p');
+const bodyElement = document.q
+// p.forEach(par => {
+//     console.log(par)
+//     par.addEventListener('select', (e)=>{
+//         prompt()
+//         e.target.style.color = 'red'
+//     })
+// })
+// p
