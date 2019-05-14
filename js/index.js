3479b82289c4e02b97de1348549bd3291db9812d
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-14 12:45:38 +0100

    implemented mouseover on logo-heading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', (e)=>{
+    e.target.classList.toggle('focusClass')
+})
